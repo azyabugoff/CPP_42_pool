b01ab0397e7f0729100833152561720bc1653094
azyabugoff <azik-bagov@mail.ru>
2021-09-10 21:54:41 +0300

    started CPP08 ex01

--- CPP_module_08/span_ex01/main.cpp
+++ CPP_module_08/span_ex01/main.cpp
@@ -3,16 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sesnowbi <sesnowbi@student.21-school.ru    +#+  +:+       +#+        */
+/*   By: sesnowbi <sesnowbi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 23:21:04 by sesnowbi          #+#    #+#             */
-/*   Updated: 2021/09/08 23:22:13 by sesnowbi         ###   ########.fr       */
+/*   Updated: 2021/09/10 21:53:52 by sesnowbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "span.hpp"
+#include <iostream>
 
 int main()
 {
+    span obj1(5);
+    std::vector<int> vec1 = obj1.getVec();
+    for (int i = 0; i < vec1.size(); ++i)
+        std::cout << vec1[i] << std::endl;
     return (0);
 }
--- CPP_module_08/span_ex01/span.cpp
+++ CPP_module_08/span_ex01/span.cpp
@@ -3,11 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   span.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sesnowbi <sesnowbi@student.21-school.ru    +#+  +:+       +#+        */
+/*   By: sesnowbi <sesnowbi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 23:23:30 by sesnowbi          #+#    #+#             */
-/*   Updated: 2021/09/08 23:23:38 by sesnowbi         ###   ########.fr       */
+/*   Updated: 2021/09/10 20:34:48 by sesnowbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "span.hpp"
+
+span::span() : N(0), vec(N) {}
+
+span::span(int N) : N(N), vec(N) {}
+
+span::span(const span &obj) : N(obj.N), vec(obj.vec) {}
+
+span &span::operator = (const span &obj)
+{
+	if (this == &obj)
+		return (*this);
+	N = obj.N;
+	vec = obj.vec;
+	return (*this);
+}
+
+int span::size() const { return (N); }
+
+std::vector<int> span::getVec() const { return (vec); }
